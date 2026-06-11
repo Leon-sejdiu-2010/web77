c68650bff35ddde37e695c582844dcb0ae76c07f
Leon-sejdiu-2010 <leonsejdiu29@gmail.com>
2026-06-11 17:47:49 +0200

    comit

---
+++ module26/js/main.js
@@ -0,0 +1,56 @@
+console.log("hello from leoni")
+var x=5;
+var y=28+43;
+console.log(y);
+
+var b=6%3;
+console.log(b);
+
+var c=99;
+c++;
+console.log(c);
+
+var c=99;
+c--;
+console.log(c);
+
+var q=4;
+var p=q**2;
+console.log(p);
+
+
+var b=6;
+b-=2;
+console.log(b);
+
+var b=6;
+b*=2;
+console.log(b);
+
+
+
+var b=7;
+var a="7";
+console.log(a===b);
+
+var b=6;
+var a=74;
+console.log(a>b);
+
+var b=6;
+var a=74;
+console.log(a<b);
+
+
+var b=6;
+var a=74;
+console.log(a!=b);
+
+var b=6;
+var a=6;
+console.log(a!=b);
+
+var name="leoni";
+var lastname="sejdiu";
+var fullname=name+" "+lastname;
+console.log(fullname);
